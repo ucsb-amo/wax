43f5b2f88cee19581ee6709364b34c9b11cc8719
bowjun <weldlabucsb@gmail.com>
2025-10-17 09:03:43 -0700

    woo

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ from setuptools import setup, find_packages
 setup(
     name='wax',
     version='0.0.1',
+    url='https://github.com/ucsb-amo/wax-ucsb',
+    author='Jared Pagett',
+    author_email='pagett.jared@gmail.com',
     packages=find_packages(), # This will find 'my_package'
 )
