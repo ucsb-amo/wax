900bc9c524fc983387041bbdfb5eefa8277e2d47
hpbroida <weldlabucsb@gmail.com>
2025-12-10 14:05:28 -0800

    siglent params to object

--- waxx-src/waxx/control/misc/sdg6000x.py
+++ waxx-src/waxx/control/misc/sdg6000x.py
@@ -9,6 +9,17 @@ T_RPC_DELAY = 10.e-3
 
 dv = -0.1
 
+class SDG6000X_Params():
+    def __init__(self,
+                 frequency=0.,
+                 amplitude_vpp=0.,
+                 state=0,
+                 max_amplitude_vpp=0.):
+        self.frequency = frequency
+        self.amplitude_vpp = amplitude_vpp
+        self.max_amplitude_vpp = max_amplitude_vpp
+        self.state = state
+
 class SDG6000X(vxi11.Instrument):
     def __init__(self,ip):
         super().__init__(ip)
@@ -28,42 +39,44 @@ class SDG6000X(vxi11.Instrument):
         self.write(f"C{ch}:OUTP {s}")
 
 class SDG6000X_CH():
-    def __init__(self,ch,ip,
-                 core=Core,
-                 default_amplitude_vpp=0.,
-                 max_amplitude_vpp=1.):
+    def __init__(self,
+                 ch,ip,
+                 frequency,
+                 amplitude_vpp,
+                 default_state = 1,
+                 max_amplitude_vpp=1.,
+                 core=Core):
 
         self._instr = SDG6000X(ip)
         self.ch = ch
-
-        self.frequency = 0.
-        self.amplitude_vpp = default_amplitude_vpp
-        self.max_amplitude_vpp = max_amplitude_vpp
-
-        self.state = 1
+        
+        self._p = SDG6000X_Params(frequency=frequency,
+                                      amplitude_vpp=amplitude_vpp,
+                                      state=default_state,
+                                      max_amplitude_vpp=max_amplitude_vpp)
 
         self.core = core
 
     @portable
     def _stash_defaults(self):
-        self._frequency_default = self.frequency
-        self._amplitude_vpp_default = self.amplitude_vpp
+        self._frequency_default = self._p.frequency
+        self._amplitude_vpp_default = self._p.amplitude_vpp
 
     @portable
     def _restore_defaults(self):
-        self.frequency = self._frequency_default
-        self.amplitude_vpp = self._amplitude_vpp_default
+        self._p.frequency = self._frequency_default
+        self._p.amplitude_vpp = self._amplitude_vpp_default
 
     @portable
     def set_output_rpc(self,state=1,init=False):
         if init:
             sw_changed = True
         else:
-            sw_changed = bool(state) != (self.state == 1)
+            sw_changed = bool(state) != (self._p.state == 1)
 
         if sw_changed:
-            self.state = state if state >= 0. else self.state
-            self._instr._sw_output(self.ch,self.state)
+            self._p.state = state if state >= 0. else self._p.state
+            self._instr._sw_output(self.ch,self._p.state)
 
     @portable
     def fetch_state(self):
@@ -96,9 +109,9 @@ class SDG6000X_CH():
             return None
         state = parse_output_state(reply)
 
-        self.frequency = freq
-        self.amplitude_vpp = amp
-        self.state = state
+        self._p.frequency = freq
+        self._p.amplitude_vpp = amp
+        self._p.state = state
 
     @portable
     def set_rpc(self,
@@ -110,16 +123,16 @@ class SDG6000X_CH():
             freq_changed = True
             amp_changed = True
         else:
-            freq_changed = (frequency >= 0.) and (frequency != self.frequency)
-            amp_changed = (amplitude >= 0.) and (amplitude != self.amplitude_vpp)
+            freq_changed = (frequency >= 0.) and (frequency != self._p.frequency)
+            amp_changed = (amplitude >= 0.) and (amplitude != self._p.amplitude_vpp)
         if freq_changed:
-            self.frequency = frequency if frequency!=dv else self.frequency
-            self._instr._set_freq_command(self.ch,self.frequency)
+            self._p.frequency = frequency if frequency!=dv else self._p.frequency
+            self._instr._set_freq_command(self.ch,self._p.frequency)
         if amp_changed:
-            if self.amplitude_vpp > self.max_amplitude_vpp:
+            if self._p.amplitude_vpp > self._p.max_amplitude_vpp:
                 raise ValueError("Amplitdue requested for this channel is beyond configured maximum.")
-            self.amplitude_vpp = amplitude if amplitude!=dv else self.amplitude_vpp
-            self._instr._set_amp_command(self.ch,self.amplitude_vpp)
+            self._p.amplitude_vpp = amplitude if amplitude!=dv else self._p.amplitude_vpp
+            self._instr._set_amp_command(self.ch,self._p.amplitude_vpp)
 
     @kernel
     def init(self):
